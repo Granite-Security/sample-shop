17dd57fde43bdd89b0e05c350ff88287295ecd76
moldo <44651279+avrabie@users.noreply.github.com>
2026-05-17 01:26:59 +0200

    bugfix: final in the field variable is a bad idea when working with values

--- gateway/src/main/java/org/granitesecurity/gateway/config/RouterConfig.java
+++ gateway/src/main/java/org/granitesecurity/gateway/config/RouterConfig.java
@@ -1,5 +1,6 @@
 package org.granitesecurity.gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -8,16 +9,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RouterConfig {
+
+    @Value("${microservices.greetings.uri}")
+    private String greetingsServiceUri;
+
     @Bean
     RouteLocator gatewayRouter(RouteLocatorBuilder builder, TokenRelayGatewayFilterFactory tokenRelay) {
         return builder.routes()
                 .route("greetings-service", r -> r
                         .path("/api/greetings/**")
-                        .uri("http://localhost:8060"))
+                        .uri(greetingsServiceUri))
                 .route("greetings-secured", r -> r
                         .path("/api/secured/**")
                         .filters(f->f.filter(tokenRelay.apply()))
-                        .uri("http://localhost:8060"))
+                        .uri(greetingsServiceUri))
                 .build();
     }
 }
