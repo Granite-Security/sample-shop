46d7bec86f98a2bcc26e11bd5f43016d5959f199
moldo <44651279+avrabie@users.noreply.github.com>
2026-05-19 22:00:35 +0200

    addivg 2 users

--- auth-server/src/main/java/org/granitesecurity/authserver/SecurityConfig.java
+++ auth-server/src/main/java/org/granitesecurity/authserver/SecurityConfig.java
@@ -1,7 +1,26 @@
 package org.granitesecurity.authserver;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+        UserDetails iaka = User.withUsername("iaka")
+                .password("{noop}a")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(
+                user, iaka
+        );
+    }
 }
